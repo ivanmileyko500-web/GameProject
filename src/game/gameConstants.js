0d8fd53ae6fa6384f9195e2056fa6ba72a25e230
Ivan Mileyko <ivanmileyko500@gmail.com>
2025-09-07 21:07:50 +0300

    Вынес игровые константы в отдельный файл, там же будут настройки

---
+++ src/game/gameConstants.js
@@ -0,0 +1,11 @@
+class GameConstants {
+    static outherWindowSizeAddon = {width: 16, height: 39};
+    static windowSizes = {
+        index: {width: 1600, height: 750},
+    };
+    static windowsPathMapping = {
+        index: 'index.html',
+    };
+}
+
+module.exports = GameConstants;
